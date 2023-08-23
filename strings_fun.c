097c3940a750f9505f94c41ab617b6ba924dfef4
Super User <ammar.hass94@gmail.com>
2023-08-23 09:32:41 -0700

    add function to strings_fun.c

--- strings_fun.c
+++ strings_fun.c
@@ -88,3 +88,23 @@ char *_strcpy(char *dest, char *src)
 	dest[j] = src[i];
 	return (dest);
 }
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *
+ * @s: pointer to char
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s)
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+
+	else
+		return (0);
+
+}
